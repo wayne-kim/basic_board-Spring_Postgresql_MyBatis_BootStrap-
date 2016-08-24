0fffdfeb77a12ed144e10f8c5cb2e1de2268da7e
Wayne-Kim <sumel014@naver.com>
2016-08-24 22:49:32 +0900

    after, transaction practice

--- src/main/java/org/database/controller/MessageController.java
+++ src/main/java/org/database/controller/MessageController.java
@@ -3,9 +3,11 @@ package org.database.controller;
 import javax.inject.Inject;
 
 import org.database.domain.MessageVO;
+import org.database.domain.UserVO;
 import org.database.service.MessageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +30,19 @@ public class MessageController {
 		}catch(Exception e){
 			e.printStackTrace();
 			entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	
+	@RequestMapping(value = "/{mid}", method = RequestMethod.PUT)
+	public ResponseEntity<MessageVO> readMessage(@PathVariable("mid") Integer mid, @RequestBody UserVO vo){
+		
+		ResponseEntity<MessageVO>	entity = null;
+		try{
+			entity = new ResponseEntity<>(service.readMessage(vo.getUser_id(), mid), HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+			entity = new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		}
 		return entity;
 	}
--- src/main/java/org/database/service/MessageServiceImpl.java
+++ src/main/java/org/database/service/MessageServiceImpl.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.database.domain.MessageVO;
 import org.database.persistence.MessageDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -17,6 +18,7 @@ public class MessageServiceImpl implements MessageService {
 		messageDAO.create(vo);
 	}
 
+	@Transactional
 	@Override
 	public MessageVO readMessage(String user_id, Integer mid) throws Exception {
 		messageDAO.updateState(mid);
